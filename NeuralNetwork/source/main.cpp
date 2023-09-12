0f381a2f706eaae2ed2338ea3c5c35027c8eeb1c
Michael Zarkov <michael.zarkov@gmail.com>
2023-09-12 16:03:16 +0300

    Add project files.

---
+++ NeuralNetwork/source/main.cpp
@@ -0,0 +1,15 @@
+
+#include "NeuralNetTrainer.h"
+
+
+int main()
+{	
+	// Generates sample training data for the neural network.
+	/*void generateDataForAverage(const char* filePath);
+	generateDataForAverage("sampleTrainingData/trainingData.matrix");
+	generateDataForAverage("sampleTrainingData/testingData.matrix");*/
+
+	NeuralNetTrainer net;
+
+	return 0;
+}
